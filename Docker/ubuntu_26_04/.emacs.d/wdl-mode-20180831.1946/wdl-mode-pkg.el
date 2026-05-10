caca3a5250cc882c23565fa9b4ab3f0f7e9ba088
Ohad Rodeh <ohad.rodeh@ultimagen.com>
2026-05-10 13:05:19 +0300

    wip

---
+++ Docker/ubuntu_26_04/.emacs.d/wdl-mode-20180831.1946/wdl-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "wdl-mode" "20180831.1946" "WDL (Workflow Definition Language) major mode" 'nil :commit "cef86e5afc136ae5ad9324cd6e6d6f860b889bcf" :keywords '("languages") :authors '(("Xiaowei Zhan" . "zhanxw@gmail.com")) :maintainer '("Xiaowei Zhan" . "zhanxw@gmail.com") :url "http://github.com/zhanxw/wdl-mode")
